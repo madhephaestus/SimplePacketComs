1c6c195e6a1a37f84dfa42ef6141458cfc536060
Octogonapus <firey45@gmail.com>
2019-06-28 17:45:43 -0400

    Update PacketEvent.h

--- src/PacketEvent.h
+++ src/PacketEvent.h
@@ -9,6 +9,7 @@ public:
 	bool noResponse = false;
 	// Packet ID needs to be set
 	PacketEventAbstract(uint32_t id);
+	virtual ~PacketEventAbstract() {}
 	//User function to be called when a packet comes in
 	// Buffer contains data from the packet coming in at the start of the function
 	// User data is written into the buffer to send it back 
